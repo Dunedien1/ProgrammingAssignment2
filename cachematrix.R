79d1be295dfa60266d30ec8ac01fdcacba93d052
Dunedien1 <sgeorge13@crimson.ua.edu>
2015-04-25 18:36:36 -0500

    compute inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##just seting up the program - creates the matrix and stores the important variables
+##... on multiple levels using the <<- function as described below...
+## Sets the functions for inversion later on
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) { 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse 
+  ##1st important note, setting m <<- inverse#... allows inverse to be...
+  ##...used on multiple levels, not just the parent level. #it's what...
+  ##...allows it to be used in the casheSolve below.
+  getinverse <- function() m 
+  list(set = set, get = get, 
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+##1st checks to see if inversion has already been done, tells if done...
+##... and returns the completed inverted matrix
+##If not already inverted, inverts it using setinverse and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
